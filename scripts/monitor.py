44039e3d1349311ad4914edf4f7a613f07774908
diver2daze <dkurishev@gmail.com>
2015-06-21 20:56:35 +0300

    add script fro monitoring zabbix api and send data to rabbitmq

---
+++ scripts/monitor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import config
+
+import pika
+import json
+
+from pyzabbix import ZabbixAPI
+
+
+if __name__ == "__main__":
+    zapi = ZabbixAPI("http://ni2.codeabovelab.com:8080/zabbix")
+    zapi.login("admin", "zabbix")
+    print "Connected to Zabbix API Version %s" % zapi.api_version()
+
+
+
+    rabbit_connection = pika.BlockingConnection(pika.URLParameters('amqp://admin:opentsp@ni1.codeabovelab.com:5672/%2F'))
+    print "Connected to rabbit %s"% rabbit_connection.server_properties
+    channel = rabbit_connection.channel()
+    channel.exchange_declare(exchange='opentsp.log', type='fanout')
+    for host in zapi.host.get(output="extend"):
+        for item in zapi.item.get(hostid=host['hostid']):
+            item['hostid']=host['name']
+            channel.basic_publish(exchange='opentsp.log', routing_key='zabbix', body=json.dumps(item), properties=pika.BasicProperties(content_type='application/json'))
+
+    rabbit_connection.close()
