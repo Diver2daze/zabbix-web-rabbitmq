f9f8925bfc0d184e04a95ebcd84487ecfc559260
diver2daze <dkurishev@gmail.com>
2015-08-05 16:08:14 +0300

    add feature: convert lastvalue and prevalue to numbers

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-import config
-
 import pika
 import json
 
@@ -9,22 +7,26 @@ from pyzabbix import ZabbixAPI
 
 
 if __name__ == "__main__":
-	zabbix_url="http://localhost/zabbix"
-	zabbix_user="admin"
-	zabbix_pass="zabbix"
-	rabbit_url='amqp://admin:opentsp@ni1.codeabovelab.com:5672/%2F'
-
-    zapi = ZabbixAPI(zabbix_url)
-    zapi.login(zabbix_user, zabbix_pass)
+    zapi = ZabbixAPI("http://localhost/zabbix")
+    zapi.login("admin", "zabbix")
     print "Connected to Zabbix API Version %s" % zapi.api_version()
 
-    rabbit_connection = pika.BlockingConnection(pika.URLParameters(zabbix_url))
+    rabbit_connection = pika.BlockingConnection(pika.URLParameters('amqp://admin:opentsp@ni1.codeabovelab.com:5672/%2F'))
     print "Connected to rabbit %s"% rabbit_connection.server_properties
     channel = rabbit_connection.channel()
     channel.exchange_declare(exchange='opentsp.log', type='fanout')
     for host in zapi.host.get(output="extend"):
         for item in zapi.item.get(hostid=host['hostid']):
             item['hostid']=host['name']
+            try:
+            	item['lastvalue']=float(item['lastvalue'])
+            except Exception, e:
+            	item['lastvalue']=0
+            try:
+            	item['prevvalue']=float(item['prevvalue'])
+            except Exception, e:
+            	item['prevvalue']=0
+                
             channel.basic_publish(exchange='opentsp.log', routing_key='zabbix', body=json.dumps(item), properties=pika.BasicProperties(content_type='application/json'))
 
     rabbit_connection.close()
